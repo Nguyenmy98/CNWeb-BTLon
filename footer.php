78b4d12189b39d2c1491f8ac1d16e84766caabb7
NgocHa98 <44915192+NgocHa98@users.noreply.github.com>
2018-12-09 23:22:37 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,23 +1,23 @@
  <div id = "footer">        
 
+   <br>
+    <div class=gioithieu><h3><b>GIỚI THIỆU</b></h3></div>
     <br>
-    <h2><b>GIỚI THIỆU</b></h2>
-    <br>
-    <div class="row">
-        <div class="col-md-3"> <h3 style="color: blue" title = "Về Chúng Tôi">
-         Về Chúng Tôi </h3>
+    <div class="row" >
+        <div class="col-md-3" style="border:1px solid black"> <h4 style="color: blue " title = "Về Chúng Tôi">
+         Về Chúng Tôi </h4>
          
         </div>
-        <div class="col-md-3" > <h3 style="color: blue" " title = "Thỏa Thuân Sử Dụng"> Thỏa Thuân Sử Dụng </h3> 
+        <div class="col-md-3"  style="border:1px solid black" > <h3 style="color: blue " title = "Thỏa Thuân Sử Dụng"> Thỏa Thuân Sử Dụng </h3> 
     
         </div>
-        <div class="col-md-3"> <h3 style="color: blue" title = "Chính Sách Bảo Mật"> Chính Sách Bảo Mật </h3> 
+        <div class="col-md-3" style="border:1px solid black"> <h3 style="color: blue" title = "Chính Sách Bảo Mật"> Chính Sách Bảo Mật </h3> 
         <p> Cam kết bảo mật thông tin Khách hàng
         HM Cinema cam kết không cung cấp thông tin cá nhân của bất kỳ thành viên nào ngoại trừ có sự đồng ý của thành viên hoặc theo các quy định của pháp luật cho bất kỳ Công ty bên ngoài các Công ty liên kết của Tập đoàn hoặc tổ chức nào khác, 
         hoặc cung cấp cho đối tác hoặc tổ chức nghiên cứu dưới dạng không xác định các cá nhân cụ thể để phục vụ cho các nghiên cứ khoa học hoặc nghiên cứu thị trường.</p>
         </div>
     
-        <div class="col-md-3"> <h3 style="color: blue" title = "Quy Chế Hoạt Động"> Quy Chế Hoạt Động </h3> </div>
+        <div class="col-md-3" style="border:1px solid black"> <h3 style="color: blue" title = "Quy Chế Hoạt Động"> Quy Chế Hoạt Động </h3> </div>
     </div>     
    
 </div>
