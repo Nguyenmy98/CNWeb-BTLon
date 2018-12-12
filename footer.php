6f51a8213ebbfce3211c1dedde930c64cd1e6ffe
NgocHa98 <44915192+NgocHa98@users.noreply.github.com>
2018-12-12 23:14:11 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,28 +1,71 @@
- <div id = "footer">        
+ <!-- Bootstrap core CSS -->
+ <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-   <br>
-    <div class=gioithieu><h3><b>GIỚI THIỆU</b></h3></div>
-    <br>
-    <div class="row" >
-        <div class="col-md-3" style="border:1px solid black"> <h4 style="color: blue " title = "Về Chúng Tôi">
-         Về Chúng Tôi </h4>
-         
+<!-- Custom fonts for this template -->
+<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+<!-- Plugin CSS -->
+<link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="css/creative.min.css" rel="stylesheet">
+
+<section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">HM Cinema</h2>
+            <hr class="my-4">
+          </div>
         </div>
-        <div class="col-md-3"  style="border:1px solid black" > <h3 style="color: blue " title = "Thỏa Thuân Sử Dụng"> Thỏa Thuân Sử Dụng </h3> 
+      </div>
+      <div class="container">
+        <div class="row">  
+        <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fas fa-home text-primary mb-3 sr-icon-3"></i>
+              <h3 class="mb-3">Về chúng tôi</h3>
+              <p class="text-muted mb-0">Là hệ thống rạp chiếu phim hàng đầu Việt Nam, HM cinema càng ngày càng phát triển mạnh hơn nữa</p>
+            </div>
+          </div>  
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-handshake text-primary mb-3 sr-icon-1"></i>
+              <h3 class="mb-3">Bảo Mật</h3>
+              <p class="text-muted mb-0">HM Cinema cam kết không cung cấp thông tin cá nhân của bất kỳ thành viên nào ngoại trừ có sự đồng ý của thành viên </p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-grin-beam text-primary mb-3 sr-icon-2"></i>
+              <h3 class="mb-3">Thỏa thuận sử dụng</h3>
+              <p class="text-muted mb-0">Mặc dù HM cinema luôn cố gắng cập nhập thường xuyên các nội dung trên trang web, nhưng chúng tôi không đảm bảo rắng các nội dung trên đó là mới nhất. Tất cả nội dung trang web có thể thay đổi bất cứ lúc nào</p>
+            </div>
+          </div>          
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fas fa-4x fa-heart text-primary mb-3 sr-icon-4"></i>
+              <h3 class="mb-3">Made with Love</h3>
+              <p class="text-muted mb-0">Cảm ơn quý khách đã sử dụng dịch vụ của chúng tôi!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     
-        </div>
-        <div class="col-md-3" style="border:1px solid black"> <h3 style="color: blue" title = "Chính Sách Bảo Mật"> Chính Sách Bảo Mật </h3> 
-        <p> Cam kết bảo mật thông tin Khách hàng
-        HM Cinema cam kết không cung cấp thông tin cá nhân của bất kỳ thành viên nào ngoại trừ có sự đồng ý của thành viên hoặc theo các quy định của pháp luật cho bất kỳ Công ty bên ngoài các Công ty liên kết của Tập đoàn hoặc tổ chức nào khác, 
-        hoặc cung cấp cho đối tác hoặc tổ chức nghiên cứu dưới dạng không xác định các cá nhân cụ thể để phục vụ cho các nghiên cứ khoa học hoặc nghiên cứu thị trường.</p>
-        </div>
-    
-        <div class="col-md-3" style="border:1px solid black"> <h3 style="color: blue" title = "Quy Chế Hoạt Động"> Quy Chế Hoạt Động </h3> </div>
-    </div>     
-   
-</div>
-    <link rel="stylesheet" href="bootstrap.min.css">
-    <script src="jquery-3.3.1.min.js"></script>
-    <script src="bootstrap.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
 </body>
 </html>
+
